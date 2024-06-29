9ab0ab32e3eec0550c4351243d115d9325a993f5
Niko <128133376+N1k0s1@users.noreply.github.com>
2024-06-29 21:36:07 +1200

    Fixed cogs, now working, meaning I can start working on the bot again from my main repo, and get cracking on working on implementing features.

--- cogs/Greetings.py
+++ cogs/Greetings.py
@@ -1,12 +1,22 @@
+import discord
 from discord.ext import commands
 
-class Greetings(commands.Cog, name="Greeting Commands"):
+class Greetings(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="greet", help="Sends a greeting message.")
-    async def greet(self, ctx):
-        await ctx.send("Hello!")
+    @discord.slash_command()
+    async def goodbye(self, ctx):
+        await ctx.respond('Goodbye!')
+
+    @discord.user_command()
+    async def greet(self, ctx, member: discord.Member):
+        await ctx.respond(f'{ctx.author.mention} says hello to {member.mention}!')
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        await member.send('Welcome to the server!')
 
 def setup(bot):
     bot.add_cog(Greetings(bot))
--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 import discord
-from discord import app_commands
-from discord.ext import commands
 
-class Slash(commands.Cog):
-    def __init__(self, client: commands.Bot):
-        self.client = client
+bot = discord.Bot()
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print("Slash cog loaded")
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
 
-    @commands.command()
-    async def sync(self, ctx) -> None:
-        fmt = await ctx.client.tree.sync(guild=ctx.guild)
-        await ctx.send(f"Synced {len(fmt)} commands.")
+bot.load_extension('cogs.Greetings')
 
-    @app_commands.command(name="slash", description="test slash command")
-    async def ping(self, interaction: discord.Interaction):
-        bot_latency = round(self.client.latency * 1000)
-        await interaction.response.send_message(f"Pong! {bot_latency} ms.")
+@bot.slash_command(guild_ids=[1255437522629169285])
+async def sync(ctx): 
+    await bot.sync_commands()
+    await ctx.send(f"succesfully synced commands")
 
+@bot.slash_command(guild_ids=[1255437522629169285])
+async def hello(ctx):
+    await ctx.respond("Hello!")
 
-async def setup(client):
-    await client.add_cog(Slash(client), guilds=[discord.Object(id="1255437522629169285")])
+bot.run('secrettokenhere!')
